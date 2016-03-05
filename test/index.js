01f72d1c4e35288329ef6a99f65d5c2dfaea8738
rogovski <michaeljrogowski@gmail.com>
2016-03-05 11:52:19 -0500

    added uuid test. updated index.js

--- test/index.js
+++ test/index.js
@@ -1,1 +1,12 @@
-// TODO
+var common = require('../index.js');
+var assert = require('assert');
+
+
+describe('crypto: uuid', function () {
+  it(
+    'should produce uuids',
+    function () {
+      assert.equal(common.crypto.uuid.v4().length, 36);
+    }
+  );
+});
